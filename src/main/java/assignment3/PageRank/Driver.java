17adf1f3ee81b5d6fb31c0d2c9c7c60bd934b050
Suraj Nagaraj <suraj04@ccs.neu.edu>
2017-10-18 10:19:29 -0400

    created Map only job for parsing XML

---
+++ src/main/java/assignment3/PageRank/Driver.java
@@ -0,0 +1,9 @@
+package assignment3.PageRank;
+
+public class Driver {
+
+	public static void main(String[] args) throws Exception {
+		String job1_outputPath = Parser.parseXML(args);
+		System.out.println(job1_outputPath);
+	}
+}
